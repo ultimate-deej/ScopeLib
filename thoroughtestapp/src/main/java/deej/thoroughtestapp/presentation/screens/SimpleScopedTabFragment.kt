9818c08719e3fc7f1ade702d92021405b55f7415
Maxim Naumov <ultimate.deej@gmail.com>
2020-04-16 22:40:43 +0300

    Optimize imports

--- thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/screens/SimpleScopedTabFragment.kt
+++ thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/screens/SimpleScopedTabFragment.kt
@@ -2,7 +2,6 @@ package deej.thoroughtestapp.presentation.screens
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.OnClick
