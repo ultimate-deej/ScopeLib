d4a0ed39153fe8468eb7ed7827a20e296ad89e53
Maxim Naumov <ultimate.deej@gmail.com>
2020-04-17 16:34:44 +0300

    ScopeOptions: generate `equals` & `hashCode`

--- scopelibrary/src/main/java/deej/scopelib/core/toothpick/scope/ScopeOptions.kt
+++ scopelibrary/src/main/java/deej/scopelib/core/toothpick/scope/ScopeOptions.kt
@@ -49,4 +49,22 @@ data class ScopeOptions(
     }
 
     override fun toString() = "${name.simpleName}($scopeArguments)[${instanceId.take(8)}]{parent=${parentName?.simpleName}}"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ScopeOptions
+
+        if (name != other.name) return false
+        if (instanceId != other.instanceId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + instanceId.hashCode()
+        return result
+    }
 }
