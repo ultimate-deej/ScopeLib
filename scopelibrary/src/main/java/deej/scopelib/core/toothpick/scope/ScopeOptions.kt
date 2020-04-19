ce5409e4569014d1fe09b59c8f597bbb0cec20ec
Maxim Naumov <ultimate.deej@gmail.com>
2020-04-19 04:57:05 +0300

    ScopeOptions is not responsible for maintaining the hierarchy anymore

--- scopelibrary/src/main/java/deej/scopelib/core/toothpick/scope/ScopeOptions.kt
+++ scopelibrary/src/main/java/deej/scopelib/core/toothpick/scope/ScopeOptions.kt
@@ -11,43 +11,8 @@ data class ScopeOptions(
     val scopeArguments: ScopeArguments,
     val parentName: @WriteWith<ScopeAnnotationParceler> Class<out Annotation>?,
     val managedByParent: Boolean = false,
-    val instanceId: String = UUID.randomUUID().toString(),
-    var next: ScopeOptions? = null
+    val instanceId: String = UUID.randomUUID().toString()
 ) : Parcelable {
-    val tail: ScopeOptions
-        get() = next?.tail ?: this
-
-    fun appendTail(newTail: ScopeOptions) {
-        check(newTail.managedByParent) { "managedByParent is false in $newTail" }
-        if (newTail.name == name) {
-            check(newTail.instanceId == instanceId) { "Cycle detected while trying to append $newTail" }
-            return
-        }
-        val next = this.next
-        if (next != null) {
-            next.appendTail(newTail)
-        } else {
-            this.next = newTail
-        }
-    }
-
-    fun removeStartingFrom(name: Any, instanceId: String?) {
-        var node: ScopeOptions? = this
-        while (node != null) {
-            // If instanceId is specified, it must match. If not, only check name.
-            val next = node.next
-            if (next?.name == name) {
-                if (instanceId == null || next.instanceId == instanceId) {
-                    check(next.managedByParent)
-                    node.next = null
-                }
-                return
-            }
-
-            node = next
-        }
-    }
-
     override fun toString() = "${name.simpleName}($scopeArguments)[${instanceId.take(8)}]{parent=${parentName?.simpleName}}"
 
     override fun equals(other: Any?): Boolean {
