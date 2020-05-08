2fcad355e96fa20dc58125eb8eb4b8e10e8fe35d
Maxim Naumov <ultimate.deej@gmail.com>
2020-05-08 05:53:28 +0300

    Rework `ScopeLibAppScreen`

--- thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/navigation/cicerone/ScopeLibAppScreen.kt
+++ thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/navigation/cicerone/ScopeLibAppScreen.kt
@@ -1,8 +1,19 @@
 package deej.thoroughtestapp.presentation.navigation.cicerone
 
+import androidx.fragment.app.Fragment
 import deej.scopelib.core.toothpick.scope.ScopeOptions
+import deej.scopelib.core.toothpick.scope.attachScopeOptions
+import deej.scopelib.core.toothpick.scope.useScope
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
-open class ScopeLibAppScreen : SupportAppScreen() {
+abstract class ScopeLibAppScreen : SupportAppScreen() {
     open val scopeOptions: ScopeOptions? get() = null
+    protected open val usedScopeName: Any? get() = scopeOptions?.name
+
+    final override fun getFragment() = createFragment().apply {
+        scopeOptions?.let { attachScopeOptions(it) }
+        useScope(usedScopeName)
+    }
+
+    protected abstract fun createFragment(): Fragment
 }
--- thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/navigation/cicerone/Screens.kt
+++ thoroughtestapp/src/main/java/deej/thoroughtestapp/presentation/navigation/cicerone/Screens.kt
@@ -2,36 +2,30 @@ package deej.thoroughtestapp.presentation.navigation.cicerone
 
 import deej.scopelib.core.toothpick.scope.ScopeArguments
 import deej.scopelib.core.toothpick.scope.ScopeOptions
-import deej.scopelib.core.toothpick.scope.attachScopeOptions
-import deej.scopelib.core.toothpick.scope.useScope
 import deej.thoroughtestapp.core.toothpick.scope.HomeScope
 import deej.thoroughtestapp.core.toothpick.scope.RootScope
 import deej.thoroughtestapp.core.toothpick.scope.SimpleTabScope
 import deej.thoroughtestapp.core.toothpick.scope.TabsScope
 import deej.thoroughtestapp.presentation.scopearguments.SimpleTabScopeArguments
 import deej.thoroughtestapp.presentation.screens.*
+import ru.terrakok.cicerone.android.support.SupportAppScreen
 import kotlin.random.Random
 import kotlin.random.nextInt
 
 object Screens {
     object Home : ScopeLibAppScreen() {
-        override fun getFragment() = HomeFragment().also {
-            it.useScope(RootScope::class.java)
-        }
+        override val usedScopeName get() = RootScope::class.java
+        override fun createFragment() = HomeFragment()
     }
 
     class ScopedHome : ScopeLibAppScreen() {
         override val scopeOptions = ScopeOptions.withUniqueId(HomeScope::class.java, ScopeArguments.Empty, RootScope::class.java)
-
-        override fun getFragment() = ScopedHomeFragment().also {
-            it.attachScopeOptions(scopeOptions)
-        }
+        override fun createFragment() = ScopedHomeFragment()
     }
 
     object SimpleTab : ScopeLibAppScreen() {
-        override fun getFragment() = SimpleTabFragment().also {
-            it.useScope(TabsScope::class.java)
-        }
+        override val usedScopeName get() = TabsScope::class.java
+        override fun createFragment() = SimpleTabFragment()
     }
 
     class SimpleScopedTab : ScopeLibAppScreen() {
@@ -41,16 +35,14 @@ object Screens {
             HomeScope::class.java
         )
 
-        override fun getFragment() = SimpleScopedTabFragment().also {
-            it.attachScopeOptions(scopeOptions)
-        }
+        override fun createFragment() = SimpleScopedTabFragment()
     }
 
-    object ItemList : ScopeLibAppScreen() {
+    object ItemList : SupportAppScreen() {
         override fun getFragment() = ItemListFragment()
     }
 
-    object ItemDetails : ScopeLibAppScreen() {
+    object ItemDetails : SupportAppScreen() {
         override fun getFragment() = ItemDetailsFragment()
     }
 }
