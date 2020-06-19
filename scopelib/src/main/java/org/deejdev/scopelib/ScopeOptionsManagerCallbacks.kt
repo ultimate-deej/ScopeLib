759e41698b082f11d5bfa1ee023bc2777c65ab77
Maxim Naumov <ultimate.deej@gmail.com>
2020-06-20 00:30:27 +0300

    ScopeOptionsManagerCallbacks: obtain scope options via the new property

--- scopelib/src/main/java/org/deejdev/scopelib/ScopeOptionsManagerCallbacks.kt
+++ scopelib/src/main/java/org/deejdev/scopelib/ScopeOptionsManagerCallbacks.kt
@@ -3,7 +3,10 @@ package org.deejdev.scopelib
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import org.deejdev.scopelib.internal.*
+import org.deejdev.scopelib.internal.formatScopeName
+import org.deejdev.scopelib.internal.isDropping
+import org.deejdev.scopelib.internal.uniqueInstanceId
+import org.deejdev.scopelib.internal.usedScopeName
 import toothpick.ktp.KTP
 
 class ScopeOptionsManagerCallbacks(
@@ -11,11 +14,11 @@ class ScopeOptionsManagerCallbacks(
 ) : FragmentManager.FragmentLifecycleCallbacks() {
 
     override fun onFragmentPreCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
-        check(f.scopeOptions == null || f.scopeOptions?.instanceId == f.uniqueInstanceId) {
+        check(f.attachedScopeOptions == null || f.attachedScopeOptions?.instanceId == f.uniqueInstanceId) {
             "`ScopeOptions.instanceId` is not in sync with `Fragment.uniqueInstanceId`"
         }
         // If the fragment declares a scope to open, make sure the record is stored in the manager
-        f.scopeOptions?.let(scopeOptionsManager::overwrite)
+        f.attachedScopeOptions?.let(scopeOptionsManager::overwrite)
         // If the fragment specifies a scope name to use, inject
         f.usedScopeName?.let { usedScopeName ->
             scopeOptionsManager.materialize()
@@ -28,7 +31,7 @@ class ScopeOptionsManagerCallbacks(
     override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
         if (f.isDropping) {
             // The fragment is not going to be restored, close the scope it declares
-            f.scopeOptions?.let(scopeOptionsManager::removeAndClose)
+            f.attachedScopeOptions?.let(scopeOptionsManager::removeAndClose)
         }
     }
 }
