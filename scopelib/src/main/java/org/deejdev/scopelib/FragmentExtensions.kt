c1360b60a1595f60f5d0283287832d2b42695a8a
Maxim Naumov <ultimate.deej@gmail.com>
2021-04-20 12:50:52 +0300

    FragmentExtensions: return `this` in methods for convenience

--- scopelib/src/main/java/org/deejdev/scopelib/FragmentExtensions.kt
+++ scopelib/src/main/java/org/deejdev/scopelib/FragmentExtensions.kt
@@ -4,17 +4,18 @@ import androidx.fragment.app.Fragment
 import org.deejdev.scopelib.internal.ensureArguments
 import org.deejdev.scopelib.internal.scopeBlueprint
 
-inline fun <reified Name : Annotation, reified ParentName : Annotation> Fragment.attachScopeBlueprint(modulesFactory: ScopeModulesFactory, alsoUse: Boolean = true) =
+inline fun <reified Name : Annotation, reified ParentName : Annotation> Fragment.attachScopeBlueprint(modulesFactory: ScopeModulesFactory, alsoUse: Boolean = true) = apply {
     attachScopeBlueprint(ScopeBlueprint<Name, ParentName>(modulesFactory))
+}
 
-fun Fragment.attachScopeBlueprint(scopeBlueprint: ScopeBlueprint, alsoUse: Boolean = true) {
+fun Fragment.attachScopeBlueprint(scopeBlueprint: ScopeBlueprint, alsoUse: Boolean = true) = apply {
     ensureArguments().attachScopeBlueprint(scopeBlueprint, alsoUse)
 }
 
 val Fragment.attachedScopeBlueprint: ScopeBlueprint?
     get() = arguments?.scopeBlueprint
 
-fun Fragment.useScope(name: Any?) {
+fun Fragment.useScope(name: Any?) = apply {
     ensureArguments().useScope(name)
 }
 
